41c008e541178970fe68b1654a6e73c5346a2c01
Jeff Kimball <jeffkimballis@gmail.com>
2021-10-12 15:46:13 -0500

    refactor the javascript

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 80) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    }
+const currentBar = document.getElementById('current-bar')
+const testBtn = document.getElementById('test-btn')
+
+let currentDegrees = 0;
+let width = 1
+
+//Event listeners
+//---------------
+testBtn.addEventListener('click', () =>{  
+  if (currentDegrees === 0) {
+    currentDegrees = 1    
+    setInterval(currentTemp, 20)    
+  }
+})
+
+
+//Functions
+//---------------
+
+function currentTemp() {
+  if (width >= 80) {
+    clearInterval()
+    
+  } else {
+    width++
+    currentBar.style.width = width + "%"
   }
 }
+
